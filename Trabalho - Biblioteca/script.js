771d38e3c1de4f72a7f405e4af4fe2f4fa812889
yagob2 <127253988+yagob2@users.noreply.github.com>
2023-05-08 15:55:06 -0300

    Comentado

--- Trabalho - Biblioteca/script.js
+++ Trabalho - Biblioteca/script.js
@@ -3,7 +3,7 @@ const subTotalEl = document.querySelector(".shopping");
 let listaCompra = JSON.parse(localStorage.getItem("CART")) || [];
 updateCart();
 
-var livros = [
+var livros = [//É um array chamado de livros. Cada {} contem um objeto, que contem as propriedades dele.
   {
     id: 1,
     name: "Terror na orla dos caranguejos.",
@@ -148,13 +148,14 @@ var livros = [
     categoria: "acao",
   },
 ];
-
+//addEventListener adiciona um evento de clique ao botão com a classe .searchButton. Toda essa função serve para evitar que a página redirecione o usuário antes que a função
+//searchBook tenha sido concluída.
 document.querySelector(".searchButton").addEventListener("click", (e) => {
   e.preventDefault();
   document.querySelector(".searchButton").setAttribute("disabled", true);
   searchBook();
 });
-
+//Essa função leva o usuário a página de terror e desabilita o botão, já que o usuário já está nessa sessão.
 document.querySelector("#terror").addEventListener("click", (e) => {
   e.preventDefault();
   enableButtons();
@@ -196,7 +197,7 @@ document.querySelector("#home").addEventListener("click", (e) => {
   document.querySelector("#home").setAttribute("disabled", true);
   buttonHome();
 });
-
+//Habilita todos os botões após o usuário clicar em algum deles. Serve para o usuário mudar de categoria.
 const enableButtons = () => {
   document.querySelector("#home").removeAttribute("disabled");
   document.querySelector("#terror").removeAttribute("disabled");
@@ -205,7 +206,23 @@ const enableButtons = () => {
   document.querySelector("#fantasia").removeAttribute("disabled");
   document.querySelector("#acao").removeAttribute("disabled");
 };
+<<<<<<< Updated upstream
 
+=======
+//Atualiza a página para a home.
+const buttonHome = () => {
+  var htmlString = "";
+
+  document.querySelector(".headerBotton").id = "homeB";
+  document.querySelector("main").className = "home";
+  changeButtons();
+  document.querySelector(".livros").innerHTML = htmlString;
+};
+//Inicia quando o usuário clica no botão terror. A primeira coisa é declarar uma variável chamada htmlString, que será usada para armazenar o código HTML que será gerado para exibir os livros.
+//Filtra a lista de livros para encontrar os livros que estão na categoria terror e armazena na variável terrorAba.
+//A função percorrer cada livro na lista usando o método forEach e gera código HTML para exibir as informações.
+//Atualiza a classe da tag main para terror e adiciona o código HTML gerado à div livros da página.
+>>>>>>> Stashed changes
 const buttonTerror = () => {
   var htmlString = "";
 
@@ -300,7 +317,82 @@ const buttonRomance = () => {
 
   document.querySelector(".livros").innerHTML = htmlString;
 };
+<<<<<<< Updated upstream
 
+=======
+//muda a cor do cabeçalho das abas. Reflete o tema desejado nas abas.
+function changeButtons() {
+  if (document.querySelector(".headerBotton").id == "terrorB") {
+    document.querySelector(".homeBtn").src = "/inputs/terror_p.png";
+    document.querySelector(".terrorBtn").src = "/inputs/terror_t.png";
+    document.querySelector(".mangaBtn").src = "/inputs/terror_m.png";
+    document.querySelector(".romanceBtn").src = "/inputs/terror_r.png";
+    document.querySelector(".fantasiaBtn").src = "/inputs/terror_f.png";
+    document.querySelector(".acaoBtn").src = "/inputs/terror_a.png";
+  }
+  if (document.querySelector(".headerBotton").id == "homeB") {
+    document.querySelector(".homeBtn").src = "/inputs/principal_p.png";
+    document.querySelector(".terrorBtn").src = "/inputs/principal_t.png";
+    document.querySelector(".mangaBtn").src = "/inputs/principal_m.png";
+    document.querySelector(".romanceBtn").src = "/inputs/principal_r.png";
+    document.querySelector(".fantasiaBtn").src = "/inputs/principal_f.png";
+    document.querySelector(".acaoBtn").src = "/inputs/principal_a.png";
+  }
+  if (document.querySelector(".headerBotton").id == "mangaB") {
+    document.querySelector(".homeBtn").src = "/inputs/manga_p.png";
+    document.querySelector(".terrorBtn").src = "/inputs/manga_t.png";
+    document.querySelector(".mangaBtn").src = "/inputs/manga_m.png";
+    document.querySelector(".romanceBtn").src = "/inputs/manga_r.png";
+    document.querySelector(".fantasiaBtn").src = "/inputs/manga_f.png";
+    document.querySelector(".acaoBtn").src = "/inputs/manga_a.png";
+  }
+  if (document.querySelector(".headerBotton").id == "fantasiaB") {
+    document.querySelector(".homeBtn").src = "/inputs/fantasia_p.png";
+    document.querySelector(".terrorBtn").src = "/inputs/fantasia_t.png";
+    document.querySelector(".mangaBtn").src = "/inputs/fantasia_m.png";
+    document.querySelector(".romanceBtn").src = "/inputs/fantasia_r.png";
+    document.querySelector(".fantasiaBtn").src = "/inputs/fantasia_f.png";
+    document.querySelector(".acaoBtn").src = "/inputs/fantasia_a.png";
+  }
+  if (document.querySelector(".headerBotton").id == "romanceB") {
+    document.querySelector(".homeBtn").src = "/inputs/romance_p.png";
+    document.querySelector(".terrorBtn").src = "/inputs/romance_t.png";
+    document.querySelector(".mangaBtn").src = "/inputs/romance_m.png";
+    document.querySelector(".romanceBtn").src = "/inputs/romance_r.png";
+    document.querySelector(".fantasiaBtn").src = "/inputs/romance_f.png";
+    document.querySelector(".acaoBtn").src = "/inputs/romance_a.png";
+  }
+  if (document.querySelector(".headerBotton").id == "acaoB") {
+    document.querySelector(".homeBtn").src = "/inputs/acao_p.png";
+    document.querySelector(".terrorBtn").src = "/inputs/acao_t.png";
+    document.querySelector(".mangaBtn").src = "/inputs/acao_m.png";
+    document.querySelector(".romanceBtn").src = "/inputs/acao_r.png";
+    document.querySelector(".fantasiaBtn").src = "/inputs/acao_f.png";
+    document.querySelector(".acaoBtn").src = "/inputs/acao_a.png";
+  }
+}
+//Clica no botão para aparecer as categorias na versão mobile.
+const toggleButton = document.querySelector("#toggleButton");
+
+toggleButton.addEventListener("click", () => {
+  if (document.querySelector(".headerBotton").style.display == "none") {
+    document.querySelector(".headerBotton").style.display = "flex";
+  } else {
+    document.querySelector(".headerBotton").style.display = "none";
+  }
+});
+// Mesma função anterior mas para barra de procura.
+const toggleButton2 = document.querySelector("#toggleButton2");
+
+toggleButton2.addEventListener("click", () => {
+  if (document.querySelector(".searchTerm").style.display == "none") {
+    document.querySelector(".searchTerm").style.display = "flex";
+  } else {
+    document.querySelector(".searchTerm").style.display = "none";
+  }
+});
+//Função mostra sugestão do que o usuário procura caso ele digite mais de 2 caracteres. Caso não haja 3 caracteres o botão de pesquisa é desabilitado e não há sugestões.
+>>>>>>> Stashed changes
 document.querySelector(".searchTerm").addEventListener("input", (e) => {
   document.querySelector(".searchButton").setAttribute("disabled", true);
   if (e.target.value.length >= 2) {
@@ -310,7 +402,9 @@ document.querySelector(".searchTerm").addEventListener("input", (e) => {
     closeSuggestions();
   }
 });
-
+//Define a função handleSuggestions. Essa função filtra a lista de livros com base no valor de pesquisa e cria uma sugestão de livro correspondente.
+//Cada sugestão de livro é apresentada com uma imagem do livro e o nome do livro como um link.
+//Por fim, a função atualiza o conteúdo id="sugestoes" para exibir a lista de sugestões e altera o estilo do elemento para exibi-lo como uma caixa suspensa.
 const handleSuggestions = (searchValue) => {
   const filteredbooks = livros.filter((livro) =>
     livro.name.toLowerCase().includes(searchValue.toLowerCase())
@@ -324,18 +418,29 @@ const handleSuggestions = (searchValue) => {
   document.querySelector("#sugestoes").innerHTML = htmlString;
   document.querySelector("#sugestoes").style.display = "flex";
 };
+<<<<<<< Updated upstream
 
 const selectLivro = (livroName) => {
+=======
+//Essa função é chamada quando um usuário seleciona uma sugestão de busca e executa a busca correspondente.
+const selectLivro = (livroName, categoria) => {
+>>>>>>> Stashed changes
   document.querySelector(".searchTerm").value = livroName;
   document.querySelector(".searchButton").setAttribute("disabled", true);
   searchBook();
   closeSuggestions();
 };
+<<<<<<< Updated upstream
 
 const closeSuggestions = () => {
   document.querySelector("#sugestoes").style.display = "none";
 };
 
+=======
+//A função é responsável por filtrar a lista de livros com base na palavra digitada pelo usuário. 
+//Cria uma nova lista filteredBooks que contem os livros que tem o nome correspondente a palavra digitada.
+//Se na lista tiver ao menos um livro, é adicionado uma string HTML que contém o nome, imagem, preço e botão de compra. Se estiver vazio, aparece a mensagem nenhum livro encontrado.
+>>>>>>> Stashed changes
 const searchBook = () => {
   var htmlString = "";
   const searchValue = document.querySelector(".searchTerm").value;
@@ -369,7 +474,7 @@ const buttonHome = () => {
 
   document.querySelector(".livros").innerHTML = htmlString;
 };
-
+//Função para alternar entre exibido e oculto o carrinho toda vez que o usuário clicar no botão do carrinho.
 function toggleShopCart() {
   if (document.querySelector(".Compras").style.display == "none") {
     document.querySelector(".Compras").style.display = "flex";
@@ -377,7 +482,7 @@ function toggleShopCart() {
     document.querySelector(".Compras").style.display = "none";
   }
 }
-
+//Função para adicionar o item na lista de compras. Se já exisitir o item, ele soma. Caso contrário adiciona o item novo.
 function addToCart(id) {
   if (listaCompra.some((item) => item.id === id)) {
     changeNumberOfUnits("plus", id);
@@ -392,6 +497,7 @@ function addToCart(id) {
 
   updateCart();
 }
+<<<<<<< Updated upstream
 
 function updateCart() {
   document.querySelector("#end").setAttribute("disabled", true);
@@ -401,6 +507,10 @@ function updateCart() {
   localStorage.setItem("CART", JSON.stringify(listaCompra));
 }
 
+=======
+//Função para calcular o total de itens no carinho e o preço total. Se o preço > 0, aparece o botão de finalizar
+//totalPrice.toFixed(2) - arredonda para duas casas decimais.
+>>>>>>> Stashed changes
 function renderSubTotal() {
   let totalPrice = 0;
   let totalItems = 0;
@@ -415,7 +525,7 @@ function renderSubTotal() {
 
   subTotalEl.innerHTML = `<h3>Total: R$ ${totalPrice.toFixed(2)}</h3>`;
 }
-
+//Responsável por atualizar a exibição dos itens adicionados ao carrinho. Adiciona o botão de somar e diminuir a quantidade de itens
 function renderCartItems() {
   cartItemsEl.innerHTML = "";
   listaCompra.forEach((item) => {
@@ -429,13 +539,13 @@ function renderCartItems() {
     </div>`;
   });
 }
-
+//Remover o item do carrinho
 function removeItemFromCart(id) {
   listaCompra = listaCompra.filter((item) => item.id !== id);
 
   updateCart();
 }
-
+//Responsável por aumentar ou diminuir o número de itens no carrinho. Utiliza o map para criar novo array com a mesma quantidade do array listaCompra.
 function changeNumberOfUnits(action, id) {
   listaCompra = listaCompra.map((item) => {
     let numberOfUnits = item.numberOfUnits;
@@ -454,26 +564,49 @@ function changeNumberOfUnits(action, id) {
 
   updateCart();
 }
-
+//Finaliza a compra e informa o valor
 function endShopping() {
   localStorage.clear();
   alert("Compra finalizada com Sucesso");
   subTotalEl.innerHTML = `<h3>Total: R$ 0.00</h3>`;
   window.location.reload();
 }
-
+//Atualiza a página para o home e limpa o carrinho
 window.onload = function () {
   updateCart();
 
   document.querySelector(".searchButton").setAttribute("disabled", true);
 };
+<<<<<<< Updated upstream
 
 let clip = document.querySelector(".vid");
+=======
+//Função para atualizar o carrinho
+function updateCart() {
+  document.querySelector("#end").setAttribute("disabled", true);
+  renderCartItems();
+  renderSubTotal();
+>>>>>>> Stashed changes
 
 clip.addEventListener("mouseover", function (e) {
   clip.play();
 });
 
+<<<<<<< Updated upstream
 clip.addEventListener("mouseout", function (e) {
   clip.pause();
 });
+=======
+var tocar = false;
+//Chama 
+function playAudio(idDiv) {
+  var audio = document.querySelector("#imagemLivro" + idDiv + " audio");
+  if (tocar == false) {
+    audio.play();
+    tocar = true;
+  } else {
+    audio.pause();
+    tocar = false;
+  }
+}
+>>>>>>> Stashed changes
